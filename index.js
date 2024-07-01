66cc71beb784a35284282b9aeee9f132fa64db6c
Saadat-Saman <saadat.saman1998@gmail.com>
2024-07-01 16:24:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ let guestScore = 0;
 
 function scoreIncrease(team, score) {
     if (team === "home") {
-        homeScore += score
-        document.getElementById("home-score").innerHTML = homeScore
+        homeScore += score;
+        document.getElementById("home-score").innerHTML = homeScore;
     }else if (team === "guest") {
-        guestScore += score
-        document.getElementById("guest-score").innerHTML = guestScore
+        guestScore += score;
+        document.getElementById("guest-score").innerHTML = guestScore;
     }
 }
 function reset() {
-    document.getElementById("home-score").innerHTML = 0
-    document.getElementById("guest-score").innerHTML = 0
+    document.getElementById("home-score").innerHTML = 0;
+    document.getElementById("guest-score").innerHTML = 0;
     homeScore = 0;
     guestScore = 0;
 
